4f36c295d5a4ba3dd752a2117ccda61a047008dd
liuqingdi <liuqingdi907@gmail.com>
2025-03-04 12:43:00 +0800

    file: 更新url

--- article/urls.py
+++ article/urls.py
@@ -6,17 +6,17 @@ app_name = 'article'
 
 urlpatterns = [
     # 文章列表
-    path('article-list/', views.article_list, name='article_list'),
+    path('articles-list/', views.article_list, name='article_list'),
     # 文章详情
-    path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
+    path('articles-detail/<int:id>/', views.article_detail, name='article_detail'),
     # 写文章
-    path('article-create/', views.article_create, name='article_create'),
+    path('articles-create/', views.article_create, name='article_create'),
     # 删除文章
-    path('article-delete/<int:id>/', views.article_delete, name='article_delete'),
+    path('articles-delete/<int:id>/', views.article_delete, name='article_delete'),
     # 安全删除文章
-    path('article-safe-delete/<int:id>/', views.article_safe_delete, name='article_safe_delete'),
+    path('articles-safe-delete/<int:id>/', views.article_safe_delete, name='article_safe_delete'),
     # 更新文章
-    path('article-update/<int:id>/', views.article_update, name='article_update'),
+    path('articles-update/<int:id>/', views.article_update, name='article_update'),
     # 点赞 +1
     path('increase-likes/<int:id>/', views.IncreaseLikesView.as_view(), name='increase_likes'),
     # 自己发的博客
